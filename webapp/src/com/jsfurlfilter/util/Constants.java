823af9883630cacfe1c4431c1efe5177e801ca9b
Ashwin Nambiar <apnpassion@gmail.com>
2013-02-24 23:33:25 +0530

    minor fix

--- webapp/src/com/jsfurlfilter/util/Constants.java
+++ webapp/src/com/jsfurlfilter/util/Constants.java
@@ -3,6 +3,6 @@ package com.jsfurlfilter.util;
 public class Constants {
 
 	public static final String REQUEST_ATTR_URL = "url";
-	public static final String REQUEST_ATTR_URL_CONTEXT = "url";
+	public static final String REQUEST_ATTR_URL_CONTEXT = "urlContext";
 	
 }
